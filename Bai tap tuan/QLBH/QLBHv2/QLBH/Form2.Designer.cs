70d2cd780f0a173563150ee4a8f6978c98b2e76e
NormieAhihi <127420196+NormieAhihi@users.noreply.github.com>
2024-02-06 08:28:18 +0700

    Add files via upload
    Update

---
+++ Bai tap tuan/QLBH/QLBHv2/QLBH/Form2.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace QLBH
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvDMVatTu = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtMaVatTu = new System.Windows.Forms.TextBox();
+            this.txtTenVatTu = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.cbbDonViTinh = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.MaVTu = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TenVTu = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnThem = new System.Windows.Forms.Button();
+            this.btnSua = new System.Windows.Forms.Button();
+            this.btnXoa = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDMVatTu)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dgvDMVatTu
+            // 
+            this.dgvDMVatTu.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDMVatTu.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.MaVTu,
+            this.TenVTu});
+            this.dgvDMVatTu.Location = new System.Drawing.Point(55, 328);
+            this.dgvDMVatTu.Name = "dgvDMVatTu";
+            this.dgvDMVatTu.RowHeadersWidth = 51;
+            this.dgvDMVatTu.RowTemplate.Height = 24;
+            this.dgvDMVatTu.Size = new System.Drawing.Size(855, 276);
+            this.dgvDMVatTu.TabIndex = 0;
+            this.dgvDMVatTu.CellMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dgvDMVatTu_CellMouseClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.label1.Location = new System.Drawing.Point(225, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(459, 36);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Quản lý thông tin danh mục vật tư";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Mã vật tư";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(33, 154);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 16);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Tên vật tư";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(392, 62);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 16);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Đơn vị tính";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(392, 153);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(67, 16);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Phần trăm";
+            // 
+            // txtMaVatTu
+            // 
+            this.txtMaVatTu.Location = new System.Drawing.Point(117, 56);
+            this.txtMaVatTu.Name = "txtMaVatTu";
+            this.txtMaVatTu.Size = new System.Drawing.Size(241, 22);
+            this.txtMaVatTu.TabIndex = 6;
+            // 
+            // txtTenVatTu
+            // 
+            this.txtTenVatTu.Location = new System.Drawing.Point(117, 147);
+            this.txtTenVatTu.Name = "txtTenVatTu";
+            this.txtTenVatTu.Size = new System.Drawing.Size(241, 22);
+            this.txtTenVatTu.TabIndex = 7;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(481, 147);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(241, 22);
+            this.textBox3.TabIndex = 8;
+            // 
+            // cbbDonViTinh
+            // 
+            this.cbbDonViTinh.FormattingEnabled = true;
+            this.cbbDonViTinh.Location = new System.Drawing.Point(481, 62);
+            this.cbbDonViTinh.Name = "cbbDonViTinh";
+            this.cbbDonViTinh.Size = new System.Drawing.Size(241, 24);
+            this.cbbDonViTinh.TabIndex = 9;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtMaVatTu);
+            this.groupBox1.Controls.Add(this.cbbDonViTinh);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txtTenVatTu);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Location = new System.Drawing.Point(55, 67);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(855, 226);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Thông tin chung";
+            // 
+            // MaVTu
+            // 
+            this.MaVTu.DataPropertyName = "MaVTu";
+            this.MaVTu.HeaderText = "Mã vật tư";
+            this.MaVTu.MinimumWidth = 6;
+            this.MaVTu.Name = "MaVTu";
+            this.MaVTu.Width = 125;
+            // 
+            // TenVTu
+            // 
+            this.TenVTu.DataPropertyName = "TenVTu";
+            this.TenVTu.HeaderText = "Tên vật tư";
+            this.TenVTu.MinimumWidth = 6;
+            this.TenVTu.Name = "TenVTu";
+            this.TenVTu.Width = 125;
+            // 
+            // btnThem
+            // 
+            this.btnThem.Location = new System.Drawing.Point(77, 653);
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(75, 23);
+            this.btnThem.TabIndex = 11;
+            this.btnThem.Text = "Thêm";
+            this.btnThem.UseVisualStyleBackColor = true;
+            // 
+            // btnSua
+            // 
+            this.btnSua.Location = new System.Drawing.Point(221, 653);
+            this.btnSua.Name = "btnSua";
+            this.btnSua.Size = new System.Drawing.Size(75, 23);
+            this.btnSua.TabIndex = 12;
+            this.btnSua.Text = "Sửa";
+            this.btnSua.UseVisualStyleBackColor = true;
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Location = new System.Drawing.Point(337, 653);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(75, 23);
+            this.btnXoa.TabIndex = 13;
+            this.btnXoa.Text = "Xóa";
+            this.btnXoa.UseVisualStyleBackColor = true;
+            this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(978, 714);
+            this.Controls.Add(this.btnXoa);
+            this.Controls.Add(this.btnSua);
+            this.Controls.Add(this.btnThem);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgvDMVatTu);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDMVatTu)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvDMVatTu;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtMaVatTu;
+        private System.Windows.Forms.TextBox txtTenVatTu;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.ComboBox cbbDonViTinh;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn MaVTu;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TenVTu;
+        private System.Windows.Forms.Button btnThem;
+        private System.Windows.Forms.Button btnSua;
+        private System.Windows.Forms.Button btnXoa;
+    }
+}
---
+++ Bai tap tuan/QLBH/QLBHv2/QLBH/Form2.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QLBH
+{
+    public partial class Form2 : Form
+    {
+        DataAccess dataAccess = new DataAccess();
+        public Form2()
+        {
+            InitializeComponent();
+            
+            LoadData();
+
+            cbbDonViTinh.DataSource = dataAccess.GetDataTable("select distinct DvTinh from VatTu");
+            cbbDonViTinh.DisplayMember = "DvTinh";
+            cbbDonViTinh.ValueMember = "DvTinh";
+
+
+        }
+        void LoadData()
+        {
+            dgvDMVatTu.DataSource = dataAccess.GetDataTable("select * from VatTu");
+        }
+
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            DataGridViewRow _dr = dgvDMVatTu.SelectedRows[0];
+            string _MaVatTu = _dr.Cells["MaVTu"].Value.ToString();
+            MessageBox.Show(_MaVatTu);
+
+            dataAccess.UpdateData(string.Format("delete from VATTU where MaVTu ='{0}'", _MaVatTu));
+            LoadData();
+
+        }
+
+        private void dgvDMVatTu_CellMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            DataGridViewRow _dr = dgvDMVatTu.SelectedRows[0];
+            string _MaVatTu = _dr.Cells["MaVTu"].Value.ToString();
+            string _TenVatTu = _dr.Cells["TenVTu"].Value.ToString();
+
+            txtMaVatTu.Text = _MaVatTu;
+            txtTenVatTu.Text = _TenVatTu;
+        }
+    }
+}
